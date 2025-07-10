f7e13ec5c3c2bd24b1b4e01c2dc0124a7ee92bb8
danidan902 <danid93575@gmail.com>
2025-07-10 02:49:52 -0700

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -29,7 +29,7 @@ function ContactSection() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://daniel-web-two.vercel.app/api/contact", {
+      const res = await fetch("https://daniel-web-api.vercel.app/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
