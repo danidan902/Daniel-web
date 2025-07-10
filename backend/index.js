8d7109f67aafa819eadc6bf93527b85a475711d3
danidan <danid93575@gmail.com>
2025-07-10 01:28:28 -0700

    Setup CORS for deployed frontend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //  Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://daniel-web-two.vercel.app",
+  methods: ["POST", "GET"]
+}));
 app.use(express.json());
 
 //   MongoDB Connected
